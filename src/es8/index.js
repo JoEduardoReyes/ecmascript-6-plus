d494988cf99a8b606ee9597de33ffd026d9b7cc8
Eduardo Reyes <joeduardoreyes@outlook.com>
2021-08-23 17:02:30 -0600

    que paso con ES9

--- src/es8/index.js
+++ src/es8/index.js
@@ -28,3 +28,31 @@ console.log(values.length);
 const hello = "hello";
 console.log(hello.padStart(7, "hi"));
 console.log(hello.padEnd(12, " -----"));
+
+// Asyn await
+
+const helloWorld = () => {
+  return new Promise((resolve, reject) => {
+    true
+      ? setTimeout(() => resolve("Hello World"), 3000)
+      : reject(new Error("Test Error"));
+  });
+};
+
+const helloAsync = async () => {
+  const hello = await helloWorld();
+  console.log(hello);
+};
+
+helloAsync();
+
+const anotherFunction = async () => {
+  try {
+    const hello = await helloWorld;
+    console.log(hello);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+anotherFunction();
