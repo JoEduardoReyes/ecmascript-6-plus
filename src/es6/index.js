efab196cd5c733600eea7010ae6a2f9d6b8953b8
Eduardo Reyes <joeduardoreyes@outlook.com>
2021-08-23 16:23:04 -0600

    Terminamos con la revision de ECMASCRIPT 6

--- src/es6/index.js
+++ src/es6/index.js
@@ -125,3 +125,60 @@ const helloPromise = () => {
 helloPromise()
   .then((response) => console.log(response))
   .catch((error) => console.error(error));
+
+//Clases, Módulos y Generadores
+
+// clases y constructores
+class calculator {
+  constructor() {
+    this.valueA = 0;
+    this.valueB = 0;
+  }
+  sum(valueA, valueB) {
+    this.valueA = valueA;
+    this.valueB = valueB;
+    return this.valueA + valueB;
+  }
+}
+
+const calc = new calculator();
+console.log(calc.sum(3, 2));
+
+// imports y exports
+
+import { hello } from "./module";
+
+hello();
+
+// generadores
+
+function* helloWorld() {
+  if (true) {
+    yield "hello, ";
+  }
+  if (true) {
+    yield "World";
+  }
+}
+
+const generatorHello = helloWorld();
+console.log(generatorHello.next().value);
+console.log(generatorHello.next().value);
+console.log(generatorHello.next().value);
+
+function* fibonacci() {
+  var fn1 = 1;
+  var fn2 = 1;
+  while (true) {
+    var actual = fn2;
+    fn2 = fn1;
+    fn1 = fn1 + actual;
+    var reset = yield actual;
+    if (reset) {
+      fn1 = 1;
+      fn2 = 1;
+    }
+  }
+}
+
+var secuencia = fibonacci();
