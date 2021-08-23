0be4ca69c6ed68fcc3cefccbc704a1033a035808
Eduardo Reyes <joeduardoreyes@outlook.com>
2021-08-23 11:19:06 -0600

    Arrow Functions, Promesas y parametros en objetos

--- src/es6/index.js
+++ src/es6/index.js
@@ -71,3 +71,57 @@ console.log(education);
 }
 
 console.log(varglobal);
+
+// Arrow Functions, Promesas y Parámetros en objetos
+
+let name = "Eduardo";
+let age = 22;
+
+// es5
+// obj = {name = name, age = age}
+
+// es6
+obj2 = {
+  name,
+  age,
+};
+console.log(obj2);
+
+// ARROW FUNCTION
+
+const names = [
+  { name: "Eduardo", age: 22 },
+  { name: "Yesica", age: 27 },
+];
+
+let listOfName = names.map(function (item) {
+  console.log(item.name);
+});
+
+let listOfNames = names.map((item) => console.log(item.name));
+
+const listOfName3 = (name) => {
+  console.log("Hola");
+};
+
+const listOfName4 = (name) => {
+  console.log("hola");
+};
+
+const square = (num) => num * num;
+
+// Promesas
+
+const helloPromise = () => {
+  return new Promise((resolve, reject) => {
+    if (true) {
+      resolve("Hey todo bien");
+    } else {
+      reject("Ups");
+    }
+  });
+};
+
+helloPromise()
+  .then((response) => console.log(response))
+  .catch((error) => console.error(error));
